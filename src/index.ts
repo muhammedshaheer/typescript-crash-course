9f6f86d18420038c0174676b514b2791257fc878
Muhammed Shaheer <ckshaheer2012@gmail.com>
2022-02-24 19:03:21 +0530

    generics

--- src/index.ts
+++ src/index.ts
@@ -119,3 +119,10 @@ const emp = new Employee(3, 'Shawn', 'Developer')
 // console.log(emp.name);
 // console.log(emp.register());
 
+//Generics
+function getArray<T>(items: T[]): T[] {
+    return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1, 2, 3, 4])
+let strArray = getArray<string>(['Brad', 'John', 'Joe'])
